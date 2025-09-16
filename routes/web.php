c8db8e4d3caf64b8496a0df2b1ffe6b4c6fdfd3f
yogesh773 <yogeshyaduvanshi0415@gmail.com>
2025-09-16 11:09:59 +0530

    Updated routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,16 @@
 use App\Http\Controllers\flashController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Mailcontroller;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\LapController;
+use App\Http\Controllers\TurboController;
+use App\Http\Controllers\RequestController;
+use App\Http\Controllers\leet;
+
+use App\Http\Controllers\testController;
+use App\Http\Controllers\uploadController;
 
 
 
@@ -12,9 +22,20 @@ use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::view('aptest','request'); // Show the form
+Route::post('aptest',[RequestController::class,'login']); // Handle form submit
+
+Route::post('login',[leet::class,'login']);
+Route::view('login','login');
+
+Route::view('since','since');
+Route::get('logout',[leet::class,'logout']);
 
 Route::post('add',[flashController::class,'adduser']);
 Route::view('flash','flash');
 
 
+
+Route::view('upload','upload');  
+Route::post('upload',[uploadController::class,'upload']);
 
