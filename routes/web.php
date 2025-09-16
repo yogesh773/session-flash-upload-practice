bbf46e90e855d79a064753e4db781b910722048a
yogesh773 <yogeshyaduvanshi0415@gmail.com>
2025-09-16 11:23:31 +0530

    Updated routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,11 @@
 use App\Http\Controllers\flashController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Mailcontroller;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\LapController;
-use App\Http\Controllers\TurboController;
-use App\Http\Controllers\RequestController;
-use App\Http\Controllers\leet;
 
-use App\Http\Controllers\testController;
+
+use App\Http\Controllers\Session;
+
+
 use App\Http\Controllers\uploadController;
 
 
@@ -22,14 +18,12 @@ use App\Http\Controllers\uploadController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::view('aptest','request'); // Show the form
-Route::post('aptest',[RequestController::class,'login']); // Handle form submit
 
-Route::post('login',[leet::class,'login']);
+Route::post('login',[session::class,'login']);
 Route::view('login','login');
 
-Route::view('since','since');
-Route::get('logout',[leet::class,'logout']);
+Route::view('session','session');
+Route::get('session',[session::class,'session']);
 
 Route::post('add',[flashController::class,'adduser']);
 Route::view('flash','flash');
